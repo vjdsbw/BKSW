0b46856fbceb5b663cf411282fe82d0f462dc19e
vjdsbw <1459281431@qq.com>
2022-05-09 09:39:35 +0800

    'xxx''

--- router/index.js
+++ router/index.js
@@ -13,4 +13,20 @@ router.get("/index.php/images/Corporatestyle", (req, res) => {
     res.render("base.html");
 })
 
+router.get("/index.php/single/company", (req, res) => {
+    res.render("company.html");
+})
+
+router.get("/index.php/product/product", (req, res) => {
+    res.render("product.html");
+})
+
+router.get("/index.php/images/news", (req, res) => {
+    res.render("news.html");
+})
+
+router.get("/index.php/images/contact", (req, res) => {
+    res.render("contact.html");
+})
+
 module.exports = router;
