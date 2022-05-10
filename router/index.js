cdc136be1df118802649ca6979fa2b77d9ca62eb
233jn <1437316278@qq.com>
2022-05-10 10:26:46 +0800

    完善数据库

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const  News= require("../model/news")
 router.get("/", (req, res) => {
     res.redirect("/index")
 })
@@ -8,8 +8,30 @@ router.get("/", (req, res) => {
 router.get("/index", (req, res) => {
     res.render("index.html");
 })
-// 将接口news重定向到news.js里的news接口
+
+
+
+// 以下为news的接口
 router.get("/news",(req,res)=>{
     res.redirect("/news/news")
 })
+router.get("/cnews",(req,res)=>{
+    res.redirect("/news/cnews")
+})
+router.get("/hnews",(req,res)=>{
+    res.redirect("/news/hnews")
+})
+router.get("/snews",async(req,res)=>{
+    
+    var id =req.query.id;
+    console.log(id);
+    var mesa =await News.find();
+    var mes=mesa[id-1];
+    res.render("snews.html",{mes,mesa,id})
+
+})
+// 以上为news的接口
+
+
+
 module.exports = router;
--- router/news.js
+++ router/news.js
@@ -3,8 +3,17 @@ const  News= require("../model/news")
 const path = require("path")
 const mongoose = require("mongoose")
 const router = express.Router();
-
+const ObjectId = require('mongodb').ObjectID;
 router.get("/news",(req,res)=>{
+
     res.render("news.html")
 })
+router.get("/cnews",(req,res)=>{
+
+    res.render("cnews.html")
+})
+router.get("/hnews",(req,res)=>{
+    res.render("hnews.html")
+})
+
 module.exports = router;
