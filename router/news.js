24afb001dda61f3af4421d3c72634cf0f300efec
niuniu5 <mn15062116527@163.com>
2022-05-11 17:33:17 +0800

    修改product.nav

--- router/news.js
+++ router/news.js
@@ -4,45 +4,25 @@ const path = require("path")
 const mongoose = require("mongoose")
 const router = express.Router();
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
 router.get("/news",(req,res)=>{
     res.render("news.html")
 })
-router.get("/cnews",(req,res)=>{
-=======
-    res.render("news.html")
-})
-router.get("/cnews",(req,res)=>{
-=======
-router.get("/news",(req,res)=>{
-    res.render("news.html")
->>>>>>> master
 
-})
-
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/wangtao
-=======
 router.get("/cnews",(req,res)=>{
->>>>>>> master
     res.render("cnews.html")
 })
 router.get("/hnews",(req,res)=>{
     res.render("hnews.html")
 })
 router.get("/snews",async(req,res)=>{    
-    var id =req.query.id;
     var mesa =await News.find();
     var mes=mesa[id-1];
     console.log(mes.frequency);
     var temp = mes.frequency+1;
     News.updateMany({num:id},{frequency:temp},(err)=>{
-        if(err) {
+        if(err) 
         console.log(err);
-    }})
-    console.log(temp);
-    res.render("snews.html",{mes,mesa,id})
+    } )  
 })
+
 module.exports = router;
